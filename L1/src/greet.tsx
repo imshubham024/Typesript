6d2adf97820a0374b15740312e68df351b912ed1
Shubham gupta <shubham24042003@gmail.com>
2025-03-25 20:09:46 +0530

    greet

---
+++ L1/src/greet.tsx
@@ -0,0 +1,14 @@
+function greet(name:string):void{
+    console.log("Hello "+name);
+}
+
+greet("jhon doe");
+
+// arrow function
+
+let greet2=(name:string):void=>{
+    console.log("Hello "+name);
+}
+
+
+greet2("jhon doe")
