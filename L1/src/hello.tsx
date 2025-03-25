650ada5341a26d6bf3f97ca43e9354b83af862c2
Shubham gupta <shubham24042003@gmail.com>
2025-03-25 20:03:19 +0530

    fir

--- L1/src/hello.tsx
+++ L1/src/hello.tsx
@@ -1,1 +1,11 @@
-console.log('Hello, world!');
+let user:string="jhon doe";
+let age:number=25;
+let male:boolean=true;
+
+console.log(user);
+console.log(age);
+console.log(age);
+
+
+// if we try to user=25-> this will throw an error
+
